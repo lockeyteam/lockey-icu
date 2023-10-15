6129534220225142cd85273b7485ca1fa94627ed
antipeth <inopeth1@gmail.com>
2023-10-15 21:05:45 +0800

    first

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import netlify from '@astrojs/netlify/functions';
+
+
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), compress()],
+  output: 'server',
+  adapter: netlify(),
+});
