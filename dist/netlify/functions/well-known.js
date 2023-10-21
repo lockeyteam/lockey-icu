56f0e2b0b12a8e2716ae8cd47eaf3a6faf59619f
lockeyteam <123921110+lockeyteam@users.noreply.github.com>
2023-10-21 22:41:13 +0800

    Update well-known.js

--- dist/netlify/functions/well-known.js
+++ dist/netlify/functions/well-known.js
@@ -1,12 +1,14 @@
-export default async (req, context) => {
-  // function logic
-  return {
-    statusCode: 200, 
-    body: {
-      "m.server": "neo.lockey.icu:443" 
-    }
-  }
-}
+export default async () => {
+
+  const body = JSON.stringify({
+    "m.server": "neo.lockey.icu:443" 
+  });
+
+  return new Response(body, {
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+};
 
 export const config = {
   path: "/.well-known/matrix/server" 
